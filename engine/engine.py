378b4b24eed41edd9200c9a2418b5427d3ce65fd
William Zeng <61915438+willzeng274@users.noreply.github.com>
2026-09-19 12:20:11 -0400

    Evaluate larger full-K B1 tiles on the passing Hopper engine

--- engine/engine.py
+++ engine/engine.py
@@ -12,7 +12,7 @@ from wide_gemv import WideGemvLayout
 from hopper_gemm import HopperGemmLayout
 from hopper_tiles import HopperTilesLayout
 from persistent_vector import PersistentVectorLayout
-from packed_word import PackedWordLayout
+from larger_gemv import LargerGemvLayout
 from fused_cache_attention import FusedCacheAttention
 from dense_prefill import DensePrefill
 from custom_kernels import embedding_norm_kernel, residual_norm_kernel, swiglu_kernel
@@ -56,7 +56,7 @@ class Engine(BaseEngine):
                 self, self.native_layout, self._layout_deadline)
             self.native_layout = PersistentVectorLayout(
                 self, self.native_layout, self._layout_deadline)
-            self.native_layout = PackedWordLayout(
+            self.native_layout = LargerGemvLayout(
                 self, self.native_layout, self._layout_deadline)
 
         self.dense_prefill = DensePrefill(self, self._layout_deadline)
